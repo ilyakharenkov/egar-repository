92c4917184d74ce278823c76496871ca244e5de0
ilyakharenkov <ilyakharenkov@gmail.com>
2023-11-25 03:15:27 +0300

    Add project git

--- final-project/src/main/java/com/example/finalproject/domain/entity/image/Image.java
+++ final-project/src/main/java/com/example/finalproject/domain/entity/image/Image.java
@@ -1,38 +1,28 @@
 package com.example.finalproject.domain.entity.image;
 
-import com.example.finalproject.domain.entity.instrument.cutter.*;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-//Фотографии инструмента
-@Getter
-@Setter
-@NoArgsConstructor
+@Entity
+@Data
+@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Image {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String fileName;
-    private Long size;
-    private String contentType;
-    private Byte[] bytes;
 
-    //Ассоциации
-    private CylinderCutter cylinderCutters;
-    private SphericalCutter sphericalCutters;
-    private ToroidalCutter toroidalCutters;
-    private Engraver engravers;
-    private Drill drills;
-    private Alignment alignments;
-    private Countersink countersinks;
-    private CountersinkWithFlatTip countersinkWithFlatTips;
-    private Tap taps;
-    private ThreadCutter threadCutters;
-    private ReverseRadiusCutter reverseRadiusCutters;
-    private DovetailCutter dovetailCutters;
-    private SlotCutter slotCutters;
+    @Column
+    private String name;
+
+    @Column
+    private String downloadLink;
+
+
 
 }
--- final-project/src/main/java/com/example/finalproject/domain/repository/InstrumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.finalproject.domain.repository;
-
-import com.example.finalproject.domain.entity.instrument.Instrument;
-
-public interface InstrumentRepository {
-    Instrument getInstrument();
-}
