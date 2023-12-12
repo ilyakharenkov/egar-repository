c6823cb8258ec5ffe7cea6cb40c461430ff1d392
Ilia kharenkov <100045092+ilyakharenkov@users.noreply.github.com>
2023-12-12 15:06:26 +0300

    Delete final-project directory

--- final-project/src/main/java/com/example/finalproject/domain/repository/price/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.finalproject.domain.repository.price;
-
-import com.example.finalproject.domain.entity.price.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PriceRepository extends JpaRepository<Price, Long> {
-}
--- final-project/src/main/java/com/example/finalproject/service/client/ClientService.java
+++
@@ -1,8 +0,0 @@
-package com.example.finalproject.service.client;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class ClientService {
-
-}
