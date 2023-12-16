67dfe40623ea3e2978a8f64b5a92e9b1abbc5479
ilyakharenkov <ilyakharenkov@gmail.com>
2023-12-16 17:11:21 +0300

    Вынес все конфигурации в отдельный пакет.

--- final-project/src/main/java/com/example/inventoryinstrument/configuration/MvcConfiguration.java
+++ final-project/src/main/java/com/example/inventoryinstrument/configuration/MvcConfiguration.java
@@ -1,11 +1,11 @@
-package com.example.inventoryinstrument.service.client;
+package com.example.inventoryinstrument.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
