f94f2b823a48fe8a3d66ce758a3c49b081c2a61b
ilyakharenkov <ilyakharenkov@gmail.com>
2023-12-20 15:10:24 +0300

    Переименование преокта.

--- final-project/src/main/java/com/example/inventoryinstrument/image/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.inventoryinstrument.image.repository;
-
-import com.example.inventoryinstrument.image.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
--- final-project/src/main/java/com/example/inventoryinstrument/price/repository/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.inventoryinstrument.price.repository;
-
-import com.example.inventoryinstrument.price.model.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PriceRepository extends JpaRepository<Price, Long> {
-}
