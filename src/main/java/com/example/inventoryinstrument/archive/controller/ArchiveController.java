bd81a006ce7189befdfa4935ae03e935654959e5
ilyakharenkov <ilyakharenkov@gmail.com>
2023-12-25 09:19:45 +0300

    Правки

--- src/main/java/com/example/inventoryinstrument/archive/controller/ArchiveController.java
+++ src/main/java/com/example/inventoryinstrument/archive/controller/ArchiveController.java
@@ -1,22 +1,23 @@
 package com.example.inventoryinstrument.archive.controller;
 
-import com.example.inventoryinstrument.archive.model.Archive;
 import com.example.inventoryinstrument.archive.model.ArchiveDto;
 import com.example.inventoryinstrument.archive.service.ArchiveService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/archive")
 @AllArgsConstructor
 public class ArchiveController {
 
     private final ArchiveService archiveService;
 
-    @GetMapping("/archive")
+    @GetMapping
     public ResponseEntity<List<ArchiveDto>> findAll() {
         var archiveList = archiveService.findAll();
         return ResponseEntity.ok().body(archiveList);
